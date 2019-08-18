97aaa84d4d3c6540f04b008a39cb5e923705103f
moh <osman.elrashid@gmail.com>
2019-08-18 21:43:40 +0400

    initial files

---
+++ Start.bat
@@ -0,0 +1,5 @@
+call env.bat 
+cd e036_flutter_decoratedboxtransition_b 
+start ConEmu.exe -runlist cmd /K run.emulator.bat ^|^|^| cmd /K ping 127.0.0.1 -n 6 ^> nul ^& run.app.bat 
+call Code .>nul
+Exit 
---
+++ git.init.bat
@@ -0,0 +1,5 @@
+Set HUB_PROTOCOL=ssh 
+git init 
+git add . && git commit -m "initial files" 
+hub create elrashid-flutter-examples/e036_flutter_decoratedboxtransition_b 
+git push --set-upstream origin master 
